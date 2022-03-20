8158e9f81146b8f0ecaf495239e3233da6c073ba
smk7758 <18097393+smk7758@users.noreply.github.com>
2022-03-21 02:55:43 +0900

    fix building

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,36 @@ dependencies {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "16"
 }
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+}
+
+//tasks.jar {
+//    manifest {
+//        attributes["Main-Class"] = "MainKt"
+//    }
+//    configurations["compileClasspath"].forEach { file: File ->
+//        from(zipTree(file.absoluteFile))
+//    }
+//    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+//}
+
+// ref: https://johnoreilly.dev/posts/jetpack-compose-desktop-copy/
+//tasks.register<Jar>("uberJar") {
+//    archiveClassifier.set("uber")
+//
+//    from(sourceSets.main.get().output)
+//
+//    dependsOn(configurations.runtimeClasspath)
+//    from({
+//        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+//    })
+//}
 
 compose.desktop {
     application {
@@ -31,6 +59,7 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "LT-Timer"
             packageVersion = "1.0.0"
+//            outputBaseDir.set(project.buildDir.resolve("output")) // defaullt: "compose/binaries"
         }
     }
 }
